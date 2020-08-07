3b5440aaf9d234d3ef29e56952e1d1adab5f3fad
Yana Abalmaz <yana.abalmaz@gmail.com>
2020-08-08 01:33:08 +0300

    Set manager and migrations with DB

---
+++ manage.py
@@ -0,0 +1,21 @@
+from flask_script import Manager
+from flask_migrate import MigrateCommand
+
+
+from github_integration import create_app, db
+
+
+manager = Manager(create_app)
+
+manager.add_option('-c', '--config', dest='config', required=False)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def create_database():
+    """Create all database tables."""
+    db.create_all()
+
+
+if __name__ == '__main__':
+    manager.run()
